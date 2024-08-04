9bc3abdaa010b9ee2f489c4f2af6a4d5b3d89bf7
cezarltsv <cezarltsv@gmail.com>
2024-08-04 17:17:33 -0300

    feat: create users controllers

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetAll()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetById(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if(user == null) return NotFound();
+        return user;
+    }
+}
